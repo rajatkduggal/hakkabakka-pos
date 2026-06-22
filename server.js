e0f4b77dcf4ae17661ac306677411191f818fa9d
rajatkduggal <mail.marvelgroup@gmail.com>
2026-06-22 12:15:22 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
-const orderRoutes = require("./routes/orders");
-const menuRoutes = require("./routes/menu");
-const loginRoutes = require("./routes/login");
-const db = require("./config/db");
 const express = require("express");
 const cors = require("cors");
 const dotenv = require("dotenv");
 const http = require("http");
 const { Server } = require("socket.io");
+
+const orderRoutes = require("./routes/orders");
+const menuRoutes = require("./routes/menu");
+const loginRoutes = require("./routes/login");
+const db = require("./config/db");
 
 dotenv.config();
 
@@ -17,38 +18,41 @@ const io = new Server(server);
 app.use(cors());
 app.use(express.json());
 
+// Routes
 app.use("/login", loginRoutes);
 app.use("/menu", menuRoutes);
 app.use("/orders", orderRoutes);
+
+// Home route
 app.get("/", (req, res) => {
   res.send("Hakka Bakka POS Server Running");
 });
 
+// Tables route
 app.get("/tables", (req, res) => {
   const tables = [];
+
   for (let i = 1; i <= 20; i++) {
     tables.push({
       table_no: i,
       status: "Available"
     });
   }
+
   res.json(tables);
 });
-app.get("/db-test", (req, res) => {
-  db.query("SELECT * FROM restaurant_tables", (err, result) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.json(result);
-    }
+
+// Socket.io connection
+io.on("connection", (socket) => {
+  console.log("New client connected");
+
+  socket.on("disconnect", () => {
+    console.log("Client disconnected");
   });
 });
 
-io.on("connection", (socket) => {
-  console.log("User connected");
-});
-
-const PORT = process.env.PORT || 3000;
+// Port
+const PORT = process.env.PORT || 5000;
 
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
