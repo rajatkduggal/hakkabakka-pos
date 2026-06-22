0982b4154acb0f287f798f64ef97390cc17a3e1a
rajatkduggal <mail.marvelgroup@gmail.com>
2026-06-22 11:52:01 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,26 +1,23 @@
 const mysql = require("mysql2");
 require("dotenv").config();
 
-let db;
+const pool = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  waitForConnections: true,
+  connectionLimit: 10,
+  queueLimit: 0
+});
 
-try {
-  db = mysql.createConnection({
-    host: process.env.DB_HOST,
-    user: process.env.DB_USER,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_NAME
-  });
+pool.getConnection((err, connection) => {
+  if (err) {
+    console.log("Database connection failed:", err);
+  } else {
+    console.log("MySQL Pool Connected");
+    connection.release();
+  }
+});
 
-  db.connect((err) => {
-    if (err) {
-      console.log("Database not connected");
-    } else {
-      console.log("MySQL Connected");
-    }
-  });
-
-} catch (error) {
-  console.log(error);
-}
-
-module.exports = db;
+module.exports = pool.promise();
