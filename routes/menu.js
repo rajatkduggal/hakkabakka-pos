f18f5ec9a291714b19bad925dec44947be1fb3c5
rajatkduggal <mail.marvelgroup@gmail.com>
2026-06-22 13:02:06 +0530

    Update menu.js

--- routes/menu.js
+++ routes/menu.js
@@ -1,16 +1,30 @@
 const express = require("express");
 const router = express.Router();
 
-const menuItems = [
-  { name: "Dal Makhani", price_half: 90, price_full: 170 },
-  { name: "Butter Chicken", price_half: 275, price_full: 475 },
-  { name: "Mutton Rogan Josh", price_half: 350, price_full: 650 },
-  { name: "Tandoori Chicken", price_half: 230, price_full: 430 },
-  { name: "Paneer Tikka", price_half: 200, price_full: 0 }
-];
-
+// Temporary static menu for testing
 router.get("/", (req, res) => {
-  res.json(menuItems);
+  res.json([
+    {
+      name: "Butter Chicken",
+      price_half: 220,
+      price_full: 420
+    },
+    {
+      name: "Kadai Paneer",
+      price_half: 180,
+      price_full: 340
+    },
+    {
+      name: "Dal Makhani",
+      price_half: 150,
+      price_full: 280
+    },
+    {
+      name: "Tandoori Roti",
+      price_half: 15,
+      price_full: 30
+    }
+  ]);
 });
 
 module.exports = router;
