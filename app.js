374e2967db694bbc187fa893ce1ac8bea459feaf
rajatkduggal <mail.marvelgroup@gmail.com>
2026-06-22 12:38:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
 fetch("https://rdl70wqz.c36.airoapp.ai/menu")
-.then(response => response.json())
-.then(data => {
+  .then(response => response.json())
+  .then(data => {
     let output = "";
 
     data.forEach(item => {
-        output += `
-            <div>
-                <h3>${item.name}</h3>
-                <p>Half: ₹${item.price_half}</p>
-                <p>Full: ₹${item.price_full}</p>
-                <hr>
-            </div>
-        `;
+      output += `
+        <div style="padding:10px; border-bottom:1px solid #ccc;">
+          <h3>${item.name}</h3>
+          <p>Half: ₹${item.price_half}</p>
+          <p>Full: ₹${item.price_full}</p>
+        </div>
+      `;
     });
 
     document.getElementById("menu").innerHTML = output;
-})
-.catch(error => {
+  })
+  .catch(error => {
+    document.getElementById("menu").innerHTML =
+      "Error loading menu";
     console.log(error);
-});
+  });
