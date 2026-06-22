6386e43bc8d7b2b3befd82e36cafeb44a9494e00
rajatkduggal <mail.marvelgroup@gmail.com>
2026-06-22 12:32:03 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
-
 fetch("https://rdl70wqz.c36.airoapp.ai/menu")
+.then(response => response.json())
+.then(data => {
+    let output = "";
+
+    data.forEach(item => {
+        output += `
+            <div>
+                <h3>${item.name}</h3>
+                <p>Half: ₹${item.price_half}</p>
+                <p>Full: ₹${item.price_full}</p>
+                <hr>
+            </div>
+        `;
+    });
+
+    document.getElementById("menu").innerHTML = output;
+})
+.catch(error => {
+    console.log(error);
+});
